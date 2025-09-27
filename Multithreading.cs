66c4a415e431624d9b752604ad6794277a4523cd
UracosVereches <turtlerodaballo@proton.me>
2025-09-27 20:10:09 +0500

    Basic scapegoat background worker setup

--- Multithreading.cs
+++ Multithreading.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Verse;
 
 namespace BetterGrenadeHandling
 {
-    public static class Multithreading
+    public static class Scapegoat
     {
+        private static readonly BlockingCollection<Func<object>> queue = new BlockingCollection<Func<object>>();
+        private static readonly Task worker;
+
+        static Scapegoat()
+        {
+            // Background worker, runs forever
+            worker = Task.Run(() =>
+            {
+                foreach (var job in queue.GetConsumingEnumerable())
+                {
+                    try
+                    {
+                        job();
+                    }
+                    catch (Exception ex)
+                    {
+                        Log.Error($"[Better Grenade Handling] Scapegoat task failed: {ex}");
+                    }
+                }
+            });
+        }
+
+        // Add work
+        public static void Enqueue(Func<object> job)
+        {
+            queue.Add(job);
+        }
     }
 }
