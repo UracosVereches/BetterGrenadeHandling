165b7c69d33bbee735dc01115ba23acf2634e826
UracosVereches <turtlerodaballo@proton.me>
2025-09-30 02:13:52 +0500

    it's bullshitical. unnecessary overhead

--- Position_Set_Patch.cs
+++ Position_Set_Patch.cs
@@ -73,14 +73,6 @@ namespace BetterGrenadeHandling
                     }
                     int waiting_grenadierID = waiting_grenadier.thingIDNumber;
 
-                    // Assume that moved pawn can be grenadier's potential target
-                    // Continue if we found and removed target
-                    if (AttackBlacklist.RemoveTarget(waiting_grenadierID, moved_pawnID))
-                    {
-                        continue;
-                    }
-
-                    // Assume that moved pawn can be grenadier's ally.
                     // Just remove grenadier from blacklist completely since we don't know the target
                     // Because waiting grenadiers don't have any yet
                     AttackBlacklist.RemoveAttacker(waiting_grenadierID);
