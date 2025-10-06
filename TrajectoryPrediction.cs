255c8f68b340e6a2cd4c8881f30f4551634652e8
UracosVereches <turtlerodaballo@proton.me>
2025-10-06 13:56:41 +0500

    trajectory prediction wip

---
+++ TrajectoryPrediction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace BetterGrenadeHandling
+{
+    //Distance = Length(Target_Position - Firing_Position)
+    //Time = Distance / Bullet_Speed
+    //Predicted_Position = Target_Position + (Target_Velocity * Time)
+    public static class TrajectoryPrediction
+    {
+        private static Projectile projectile;
+        
+        private static void test()
+        {
+            projectile.def.projectile.Speed
+        }
+    }
+}
