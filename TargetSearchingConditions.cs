8b41f21987ccc3b3bb413cbfbbc3ae93bb7c8fcc
UracosVereches <turtlerodaballo@proton.me>
2025-10-11 17:47:45 +0500

    new target filtering system, filter for stunned targets

---
+++ TargetSearchingConditions.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.AI;
+using Verse;
+using RimWorld;
+using static UnityEngine.GraphicsBuffer;
+
+namespace BetterGrenadeHandling
+{
+    [HarmonyPatch(typeof(AttackTargetFinder), "BestAttackTarget")]
+    public static class BestAttackTarget_Prefix
+    {
+        public static void Prefix(IAttackTargetSearcher searcher, TargetScanFlags flags, ref Predicate<Thing> validator
+        )
+        {
+            Pawn searcherPawn = searcher as Pawn;
+            Verb verb = VerbCache.GetCurrentEffectiveVerb(searcher as Pawn);
+
+            Predicate<Thing> origValidator = (Thing thing) => true; // return true by default
+            // Save original validator if there's any
+            if (validator != null)
+            {
+                origValidator = validator;
+            }
+
+            // Combine original and our validator
+            Predicate<Thing> newValidator = (Thing target) => origValidator(target) && target.ShouldBeHitByEMP(verb);
+            validator = newValidator;
+
+            return;
+        }
+    }
+
+    [HarmonyPatch(typeof(Verb), "CanHitTargetFrom",
+    new Type[] { typeof(IntVec3), typeof(LocalTargetInfo)})]
+    public static class CanHitTargetFrom_Patch
+    {
+        static void Postfix(Verb __instance, IntVec3 root, LocalTargetInfo targ, ref bool __result)
+        {
+            Thing target = targ.Thing;
+
+            if (target == null)
+            {
+                return;
+            }
+
+            __result = target.ShouldBeHitByEMP(__instance);
+        }
+    }
+}
